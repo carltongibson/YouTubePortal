36f7c5c8723ef240d539bed958a3b1759278c0d1
Carlton Gibson <carlton.gibson@noumenal.co.uk>
2012-05-15 14:07:44 +0200

    Initial Commit

---
+++ YouTubePortal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YouTubePortal
+//
+//  Created by Carlton Gibson on 15/05/2012.
+//  Copyright (c) 2012 Noumenal Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YouTubePortal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YouTubePortal
+//
+//  Created by Carlton Gibson on 15/05/2012.
+//  Copyright (c) 2012 Noumenal Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
